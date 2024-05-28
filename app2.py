8a4c9b8d182b3d8144e4b08a1d02d7b152bd0086
GajareGaurav <gauravgajare2001@gmail.com>
2024-05-28 15:49:52 +0530

    Add second file to the repository

---
+++ app2.py
@@ -0,0 +1,11 @@
+# Read input from the user and convert it to an integer
+ans = int(input("Enter a number: "))
+
+# Check if the input is greater than 10
+if ans > 10:
+    # If the condition is true, print "high"
+    print("high")
+else:
+    # If the condition is false, print "low"
+    print("low")
+
