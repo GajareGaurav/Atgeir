81906489049ac4c197e3fd0acf5c4139c9b7afc4
GajareGaurav <gauravgajare2001@gmail.com>
2024-05-28 16:16:54 +0530

    This sub branch

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ ans = int(input("Enter a number: "))
 # Check if the input is greater than 10
 if ans > 10:
     # If the condition is true, print "high"
-    print("high")
+    print("very high")
 else:
     # If the condition is false, print "low"
     print("low")
